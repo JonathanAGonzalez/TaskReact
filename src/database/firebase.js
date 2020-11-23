088e6a157a9d73953d61ea5fbe70a265fa8175d0
unknown <jonathang@hotmail.com.ar>
2020-11-22 22:03:00 -0300

    add task

--- src/database/firebase.js
+++ src/database/firebase.js
@@ -3,13 +3,7 @@ import "firebase/firestore";
 
 // CONFIGURACION DE LA BASE DE DATOS EN FIREBASE
 const firebaseConfig = {
-  apiKey: "AIzaSyCn8IPRs9_kwCns95fgI3fhueOCVapw2CQ",
-  authDomain: "homework-app-jg.firebaseapp.com",
-  databaseURL: "https://homework-app-jg.firebaseio.com",
-  projectId: "homework-app-jg",
-  storageBucket: "homework-app-jg.appspot.com",
-  messagingSenderId: "290120750236",
-  appId: "1:290120750236:web:2d6c66b9903c92ca170959",
+  "aca va la config de firebase"
 };
 
 // INICIALIZAMOS LA BASE DE FIREBASE CON EL METODO INITIALIZEAPP Y LE PASAMOS LA CONFIG
